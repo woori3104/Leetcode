ee7e42f3593d551c33a0e376a67e3fcea2b16fc4
woori <sizuku11261@gmail.com>
2022-09-14 19:41:59 +0900

    Time: 5 ms, Memory: 54.1 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int i = 0;
+
+        while (i+1 < arr.length && arr[i] < arr[i+1])
+            i++;
+
+        if (i == 0 || i == arr.length-1)
+            return false;
+
+        while (i+1 < arr.length && arr[i] > arr[i+1])
+            i++;
+
+        return i == arr.length-1;
+    }
+}
